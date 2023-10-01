8f34dd9f20c8a74e7a000f073cb6f46cbf57718a
Wade Bryson <wade.j.bryson@gmail.com>
2023-10-01 14:15:44 -0500

    Heart Rate Producer done

--- Heart_Rate_Producer.py
+++ Heart_Rate_Producer.py
@@ -28,7 +28,7 @@ def offer_rabbitmq_admin_site():
         webbrowser.open_new("http://localhost:15672/#/queues")
         print()
 
-def send_message(host: str, queue1_name: str, queue2_name: str, input_file: str):
+def send_message(host: str, queue1_name: str, input_file: str):
     """
     Creates and sends a message to the queue each execution.
     This process runs and finishes.
@@ -36,7 +36,6 @@ def send_message(host: str, queue1_name: str, queue2_name: str, input_file: str)
     Parameters:
         host (str): the host name or IP address of the RabbitMQ server
         queue1_name (str): the name of the first queue
-        queue2_name (str): the name of the second queue
         input_file (str): the CSV file to be read
     """
 
@@ -50,28 +49,23 @@ def send_message(host: str, queue1_name: str, queue2_name: str, input_file: str)
         # and help ensure messages are processed in order
         # messages will not be deleted until the consumer acknowledges
         ch.queue_declare(queue=queue1_name, durable=True)
-        ch.queue_declare(queue=queue2_name, durable=True)
 
         # read CSV
         with open(input_file, 'r') as file:
             reader = csv.reader(file)
-            # skip header row
-            header = next(reader)
+            
             for row in reader:
                 # separate row into variables
-                Square_Side_Length, Perimeter, Area = row
+                Heart_Rate = row
                 # define messages
-                message1 = Perimeter
-                message2 = Area
+                message1 = Heart_Rate
                 # use the channel to publish first message to first queue
                 # every message passes through an exchange
                 ch.basic_publish(exchange="", routing_key=queue1_name, body=message1)
                 logger.info(f" [x] Sent {message1} to {queue1_name}")
-                # publish second message to second queue
-                ch.basic_publish(exchange="", routing_key=queue2_name, body=message2)
-                logger.info(f"[x] Sent {message2} to {queue2_name}")
+   
                 # wait 3 seconds before sending the next message to the queue
-                time.sleep(3)
+                time.sleep(2)
 
     except pika.exceptions.AMQPConnectionError as e:
         logger.info(f"Error: Connection to RabbitMQ server failed: {e}")
@@ -90,4 +84,4 @@ if __name__ == "__main__":
         offer_rabbitmq_admin_site()
    
     # send the message to the queue
-    send_message("localhost","Perimeter_Queue","Area_Queue", "Square_Data.csv")
+    send_message("localhost","Heart_Rate_Queue","Random_Int_Heart_Rate.csv")
